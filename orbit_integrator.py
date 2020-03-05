09f1bcbe9175c7475b8337fdf1c4ac1e4588b575
ocopping <olivercopping@live.co.uk>
2020-03-05 17:09:04 +0000

    added argument parsing for potential model and time step, changed the method of writing data to a file along with writing more variables, removed unnecessary code, added some comments

--- orbit_integrator.py
+++ orbit_integrator.py
@@ -9,23 +9,19 @@ These represent a planet orbiting around its parent star. This is done by using 
 
 import scipy as sci
 import scipy.linalg
-import astropy.constants as const
-from pprint import pprint
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
+import argparse
+
+
+#------------------------------------------------------------------------------
+
 
 # Function to define the equation of circulae velocity
 def v_circ(r):
 
     vel = sci.sqrt(G*m1/orbit_rad)
-
-    return vel
-
-# Function to define the equation of elliptical velocity
-def v_elip(r):
-
-    vel = sci.sqrt(G*m1*(2.0/r * 1/a))
 
     return vel
 
@@ -38,8 +34,6 @@ def semi_maj(r, e):
 # Function to define the equation of relative motion, which calculates the 
 # acceleration of the orbiting body
 def accel(xn):
-
-    #print('r1',r1)
 
     r = xn-r1
     r_mag = sci.linalg.norm(r) #Calculate magnitude or norm of vector
@@ -57,30 +51,23 @@ def NFW_pot_accel(xn):
     r = xn-r1
     r_mag = sci.linalg.norm(r) #Calculate magnitude or norm of vector
 
-    # accel = -1* r * ((G*M_nfw)/(sci.log(1.0+c) - c/(1.0+c))) * \
-    #     (2.0*sci.log(r_mag/rs + 1.0))/r_mag**3.0 - \
-    #     2.0/(r_mag**2.0 *rs*(r_mag/rs + 1.0)) - \
-    #     1.0/(r_mag*rs**2.0 *(r_mag/rs + 1.0)**2.0)
 
-    # accel = -1*r*((G*M_nfw)/(sci.log(1.0+c) - c/(1.0+c))) * \
-    #     ((r_mag - 2*(r_mag+rs)*sci.log((r_mag+rs)/rs)) \
-    #     / (r_mag**3 * (r_mag+rs)))
+    #accel = -r* (( c+1)*G*M_nfw / ((r_mag**3)*((sci.log(1+c)*(1+c))-c))) * (((r_mag+rs)*sci.log((r_mag+rs)/rs)-r) / (r_mag+rs))
 
-    accel = -1*r* (( c+1)*G*M_nfw*( (r_mag+rs)*sci.log((r_mag+rs)/rs)-r_mag ))\
-        / ( r_mag**2*((c+1)*sci.log(c+1)-c) * (r_mag+rs) )
+    accel = -r*G*M_nfw*(1+c) / (r_mag**3*((sci.log(1+c)*(1+c))-c) )  * ( sci.log(1+r_mag/rs) - r_mag/(r_mag+rs))
 
     return accel
 
 
 def xn_next(xn, next_half_vn, dt):
 
-    next_xn = xn + next_half_vn * dt
+    next_xn = xn + (next_half_vn * dt)
 
     return next_xn
 
 def vn_next(vn, accel, dt):
 
-    next_vn = vn + accel * dt/2.0
+    next_vn = vn + (accel * dt/2.0)
 
     return next_vn
 
@@ -91,8 +78,6 @@ def calc_time_step(central_pos,obj_pos,accel):
     dt = eta * sci.sqrt(abs(sci.linalg.norm(obj_pos)- \
         sci.linalg.norm(central_pos))/sci.linalg.norm(accel))
 
-    #print(dt)
-
     return dt
 
 def calc_energy(v, m, r):
@@ -101,11 +86,34 @@ def calc_energy(v, m, r):
 
     return E
 
+def calc_nfw_energy(v, m, xn):
 
-#iterations = 20000
+    M_nfw = 80.0 # *E10 sol masses
+    rs = 16.0 # kpc
+    c = 15.3
 
-sol_mass = const.M_sun.value
-kpc = const.kpc.value
+    r = xn-r1
+    r_mag = sci.linalg.norm(r) #Calculate magnitude or norm of vector
+
+
+    E = -G*M_nfw/r_mag *(sci.log(1+r_mag/rs)/(sci.log(1+c)-c/(1+c)))
+
+    return E
+
+
+#------------------------------------------------------------------------------
+
+
+# Take an argument for the type of potential for the simuation
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--pot_type', type=str, required=True, \
+    help="Gravitational potential model to use [Newton, NFW]")
+parser.add_argument('-t', '--time_step', type=float, required=False, \
+    help="Time step length, can be fixed (e.g. 0.0001) or dynamic (0.0)", \
+    default=0.0001)
+args = parser.parse_args()
+
+# G vakue for simplified units
 G = 43007.105731706317
 
 orbit_rad = 8.0 # kpc
@@ -113,35 +121,25 @@ apo = 8.0 # kpc
 peri = 1.0 # kpc
 e = 0.1
 
-a = semi_maj(peri,e)
-print(a)
-
 # Masses
 m1 = 9.0 # central mass in E10 solar masses
 #m2 = 1.0 # object mass in E10 solar masses
 # ^ random value
 
-t_max = 1000
+t_max = 10 # ~Gyrs
 t_current = 0.0
-const_dt = (2.0*sci.pi*orbit_rad)/v_elip(peri) * 0.01 # Gyr
-
-energies = []
-times = []
-radii = []
-velocities = []
+# const_dt = (2.0*sci.pi*orbit_rad)/v_circ(peri) * 0.01 # Gyr
 
 # Starting positions
 r1 = sci.array([0.0,0.0,0.0], dtype='float64')
-r2 = sci.array([peri,0.0,0.0], dtype='float64')
+r2 = sci.array([8.0,0.0,0.0], dtype='float64')
 
-#points = sci.repeat(r2[:,0],10)
-points = sci.array([r2,r2,r2,r2,r2])#,r2,r2,r2,r2,r2,r2,r2,r2,r2,r2]) #15 points
+points = sci.array([r2]*5)# 5 points on the line
 
 # Initial velocities
-print(v_elip(peri))
-print(v_elip(apo))
-v1 = sci.array([0.0,0.0,0.0], dtype='float64')
-v2 = sci.array([0.0,v_elip(peri),0.0], dtype='float64')
+# v1 = sci.array([0.0,0.0,0.0], dtype='float64')
+# v2 = sci.array([0.0,v_elip(peri),0.0], dtype='float64')
+v2 =  sci.array([0.0,71.0,0.0], dtype='float64')
 
 #v_com = (m1*v1+m2*v2)/(m1+m2)
 
@@ -149,6 +147,7 @@ v2 = sci.array([0.0,v_elip(peri),0.0], dtype='float64')
 fig = plt.figure()
 ax = p3.Axes3D(fig)
 line, = ax.plot([], [], [])
+time_text = ax.text(0.02, 0.95,0.01, s='', transform=ax.transAxes)
 
 # Setting the axes properties
 ax.set_xlim3d([-10.0, 10.0])
@@ -163,70 +162,79 @@ ax.set_zlabel('Z')
 ax.set_title('Orbital integrator test')
 
 
+#------------------------------------------------------------------------------
+
+
 def init():
     line.set_data([], [])
     line.set_3d_properties([])
+    time_text.set_text('')
     return line,
-
 
 ## ANIMATED ORBITAL TRAJECTORY 
 def animate_orbit(t):
 
     global r1, r2, v2, t_current, points
-    global energies, times
 
-    if t_current > t:
-
-        #print('Energies\n', energies)
-        #print('Times\n',times)
-        sci.savetxt('variables.dat', \
-            sci.transpose([times, energies, radii, velocities]), \
-            delimiter=' ', \
-            header='Time (Gyr) | Energy () | Radius (kpc) | Velocity (km/s)')
-
+    if t_current > t_max:
         exit()
 
-    accel1 = accel(r2)
-    #accel1 = NFW_pot_accel(r2)
-    dt1 = calc_time_step(r1,r2,accel1)
-    #dt1 = const_dt
 
-    #for index, dimension in enumerate(v2):
-    half_vn = vn_next(v2, accel1, dt1)
-    next_xn = xn_next(r2, half_vn, dt1)
+    # KDK algorithm
+    if args.pot_type == 'Newton':
+        accel1 = accel(r2)
+    elif args.pot_type == 'NFW':
+        accel1 = NFW_pot_accel(r2)
+
+    if args.time_step == 0.0:
+        dt = calc_time_step(r1,r2,accel1)
+    elif args.time_step > 0.0:
+        dt = float(args.time_step)
+
+    half_vn = vn_next(v2, accel1, dt)
+    next_xn = xn_next(r2, half_vn, dt)
     r2 = next_xn
 
-    #pprint(r2)
     points = sci.concatenate(([r2],points[0:-1]), axis=0)
     
-    accel2 = accel(next_xn)
-    #accel2 = NFW_pot_accel(next_xn)
-    #dt2 = calc_time_step(r1,r2,accel2)
+    if args.pot_type == 'Newton':
+        accel2 = accel(next_xn)
+    elif args.pot_type == 'NFW':
+        accel2 = NFW_pot_accel(next_xn)
 
-    v2 = vn_next(half_vn, accel2, dt1)
+    v2 = vn_next(half_vn, accel2, dt)
 
     v = sci.linalg.norm(v2)
-    velocities.append(v)
     r = sci.linalg.norm(r2)
-    radii.append(r)
-    energies.append(calc_energy(v, m1, r))
+    energy = calc_nfw_energy(v, m1, r)
         
-    t_current += dt1
-    times.append(t_current)
+    t_current += dt
+
+    # Write values to file
+    fileout.write('{0} {1} {2} {3} {4} {5} {6}\n'.format(t_current,energy,r,v,r2[0],r2[1],r2[2]))
 
     line.set_data(points[:,0], points[:,1])
     line.set_3d_properties(points[:,2])
 
+    time_text.set_text('t = {0:8.5f} Gyr'.format(t_current))
+
     return line,
 
+
+#------------------------------------------------------------------------------
+
+
 ax.scatter3D(r1[0],r1[1],r1[2],color='r',s=10)
-#plt.plot(data[:,0], data[:,1], data[:,2])
 
-# Creating the Animation object
-line_ani = animation.FuncAnimation(fig, animate_orbit, frames=int(t_max), 
-                                   init_func=init, interval=5, blit=False)
+with open('variables.dat','w') as fileout:
 
-plt.show()
+    fileout.write('# Time (Gyr) | Energy () | Radius (kpc) | Velocity (km/s) | x | y | z\n')
+
+    # Creating the Animation object
+    line_ani = animation.FuncAnimation(fig, animate_orbit, frames=int(t_max), 
+                                    init_func=init, interval=5, blit=False)
+
+    plt.show()
 
 # Set up formatting for the movie files
 #Writer = animation.writers['ffmpeg']
